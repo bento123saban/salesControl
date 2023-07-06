98efd45bb986742617712a9917c5c5a32e21f82a
Bendhard16 <82221414+bento123saban@users.noreply.github.com>
2023-07-06 14:35:05 +0900

    Add files via upload

--- js/page.js
+++ js/page.js
@@ -1,105 +1,48 @@
-                                                                                                                                                                                                                                                                                                                                                                                
-
-document.querySelectorAll(".navi").forEach(link => {
-   link.addEventListener("click", function () {
+                                                                                                                                                                                                                                                                        
+const nav   = document.querySelectorAll('.navx'),
+      pages = document.querySelectorAll('section.main-content')
       
-   let month         = new Date().getMonth()
-   let year          = new Date().getFullYear()
-   let setSch        = "schedule" + month + '' + year
-   let ScheduleNow   = JSON.parse(localStorage.getItem(setSch))
-   
-   if (!ScheduleNow) {
-      document.querySelector("#SETTING").classList.add("on")
-      document.querySelector("#settings").classList.remove("off")
-      return alert("Input Your Schedule.")
-   }
-   
-   document.querySelectorAll(".navi").forEach(nav => {nav.classList.remove("on")})      
-      this.classList.add("on")
-      document.querySelectorAll(".pages-box").forEach(box => {
-         box.classList.add("off")
-         if (box.dataset.pages.toUpperCase() == this.dataset.pages.toUpperCase()) return box.classList.remove("off")
-      })
+window.addEventListener("DOMContentLoaded", function () {
+   nav.forEach(link => {
+      link.onclick = async function () {
+         if(await findSchedule() == false) return alert('input your schedule')
+         document.querySelector("#nav-add").classList.remove('active')
+         nav.forEach(list => {list.classList.remove('active')})
+         link.classList.add('active')
+         if (this.id == 'add-icon') {
+            this.classList.add('active')
+            return document.querySelector("#nav-add").classList.add('active')
+         }
+         pages.forEach(pg => {
+            pg.classList.add('dis-none')
+            if (link.dataset.nav == pg.id) return pg.classList.remove('dis-none')
+            // console.log(this.dataset.nav)
+         })
+      }
    })
+   window.addEventListener('click', function (e) {
+      if (!e.target.matches('#add-icon-i')){
+         document.querySelector("#nav-add").classList.remove('active')
+         document.querySelector('#add-icon').classList.remove('active')
+         document.querySelector('#add-icon i').classList.remove('active')
+      }
+   })
+   
+   /* setInterval(function(){
+      
+      let second  = new Date().getSeconds()
+      let minute  = new Date().getMinutes()
+      let hour    = new Date().getHours()
+      
+      if (hour    < 10) '0' + hour
+      if (minute  < 10) '0' + minute
+      if (second  < 10) '0' + second
+      
+      const dateText    = new Date().toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})
+      const timeText    = hour + ' : ' + minute + ' : ' + second
+      
+      document.querySelector('#dashboard-date').innerHTML = dateText
+      document.querySelector('#dashboard-time').innerHTML = timeText
+      
+   }, 100) */
 })
-
-document.querySelector("#ADD").onclick = function (e) {
-   
-   let month         = new Date().getMonth()
-   let year          = new Date().getFullYear()
-   let setSch        = "schedule" + month + '' + year
-   let ScheduleNow   = JSON.parse(localStorage.getItem(setSch))
-   if (!ScheduleNow) {
-      document.querySelector("#SETTING").classList.add("on")
-      document.querySelector("#settings").classList.remove("off")
-      return alert("Input Your Schedule.")
-   }
-   
-   document.querySelector("#adds").classList.toggle('active')
-   this.classList.toggle("active")
-}
-
-document.querySelectorAll(".adds-btn").forEach(btn => {
-   btn.onclick = function (e) {
-      this.parentElement.classList.remove("active")
-      document.querySelector("#ADD").classList.add("active")
-   }
-})
-
-window.addEventListener("click", function (e) {
-   // console.log(e.target)
-   if (!e.target.matches(".add-plus")) {
-      document.querySelectorAll(".adds-btn").forEach(btn => {
-         btn.parentElement.classList.remove("active")
-      })
-      document.querySelector("#ADD").classList.add("active")
-   }
-})
-
-function toCont () {
-   
-}
-
-setInterval(() => {
-   let now  = new Date(),
-   day      = now.toLocaleDateString('en-US', { weekday: 'long' }),
-   month    = now.toLocaleDateString('en-US', { month: 'long' }),
-   date     = now.getDate(),
-   hour     = now.getHours(),
-   minute   = now.getMinutes(),
-   second   = now.getSeconds()
-   // milisecs = now.getMilliseconds()
-   
-   if (date < 10)    date     = '0' + date
-   if (hour < 10)    hour     = '0' + hour
-   if (minute < 10)  minute   = '0' + minute
-   if (second < 10)  second   = '0' + second
-   
-   return document.querySelector("#timer").innerHTML = `
-      <span>${day}, ${month} ${date}, ${now.getFullYear()}</span>
-      <span>${hour} : ${minute} : ${second}</span>
-   `
-}, 1000)
-
-function toWA (numbers) {
-   let number = '' + numbers
-   console.log(typeof number, number, number[0])
-   let waNum   = ''
-   if (number.length < 10) return alert("Less then 10 digit")
-   
-   if(number[0] != 0 && number[0] != 6 && number[0] != '+' && number[0] != 8) return alert ("Invalid number !")
-   
-   if(number[0] == 0 && number[1] == 8) {
-      waNum    = number.slice(0, number.length - 1)
-   } else if (number[0] == 6 && number[1] == 2) {
-      waNum    = number.slice(1, number.length - 1)
-   } else if (number[0] == '+' && number[1] == 6 && number[3] == 2) {
-      waNum    = number.slice(2, number.length - 1)
-   } else {
-      waNum = numbers
-   }
-   wax   = '62' + waNum
-   // return console.log(wax, waNum)
-   return window.location.href = "https://wa.me/" + wax
-}
-
--- js/schedule.js
+++ js/schedule.js
@@ -1,93 +1,140 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                    
-const nowx   = new Date(),
-   monthx    = nowx.getMonth(),
-   yearx     = nowx.getFullYear(),
-   schedulex = "Schedule" + monthx + yearx,
-   lDatex    = new Date(nowx.getFullYear(), nowx.getMonth() + 1, 0),
-   lastDatex = lDatex.getDate()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
-let HTMX    = ``
-document.querySelector(".sch-month").innerHTML = monthx + 1 + " - " + yearx
-for (i = 1; i <= lastDatex; i++) {
-   let x = i;
-   if (x < 10) x = "0" + x
-   HTMX += `
-      <div class="sch-groups mb-2">
-         <div class="sch-date">${x}</div>
-         <select id="sl${i}" class="form-select form-select-sm" aria-label=".form-select-sm example">
-            <option data-color="darkcyan" selected value=''>Select schedule</option>
-            <option data-color="red" value="OFF">OFF</option>
-            <option data-color="red" value="RO">RO</option>
-            <option data-color="red" value="AL">Cuti</option>
-            <option data-color="gold" value="M0029">Pagi M0029 - (7.20 - 18.00)</option>
-            <option data-color="goldenrod" value="M0058">Pagi M0058 - (8.20 - 19.00)</option>
-            <option data-color="orangered" value="H44">Pagi H44 - (8.20 - 20.00)</option>
-            <option data-color="blue" value="A0003">Siang A0003 - (11.20 - 22.00)</option>
-         </select>
-      </div>
-   `;
-   // console.log("sl" + i)
-}
 
-document.querySelector("#schedule-set").innerHTML = HTMX
+const scheduleList   = document.querySelector('#schedule-list'),
+   scheduleHead      = document.querySelector('#schedule-head'),
+   scheduleMonth     = new Date().toLocaleDateString(undefined, {year: 'numeric', month: 'long'}),
+   scheduleSubmit    = document.querySelector('#schedule-submit'),
+   scheduleTime      = document.querySelector('#schedule-time'),
+   timeHead          = document.querySelector('#time-head'),
+   timeSubmit        = document.querySelector('#time-submit')
 
-document.querySelectorAll("#schedule-set select").forEach(sl => {
-   sl.onchange = function () {
-      let option  = sl.options[sl.selectedIndex],
-         color    = option.dataset.color
-      sl.parentElement.firstElementChild.style.background = color
+// if(!confirm('exit')) window.location.reload()
+
+window.addEventListener('DOMContentLoaded', async function () {
+   scheduleList.innerHTML     = await SCHEDULE.setHtml()
+   scheduleHead.dataset.text  = scheduleMonth
+   
+   scheduleTime.innerHTML     = await SCHEDULE.setTimeHtml()
+   
+   window.addEventListener('change', function (e) {
+      if (e.target.matches('.schedule-select')) {
+         // console.log('changes')
+         const select      = e.target,
+            selectedColor  = select.options[select.selectedIndex].dataset.color
+            selectLabel    = select.parentElement.children[0]
+         
+         // console.log(selectedColor)
+         selectLabel.className = 'bg-' + selectedColor.toLowerCase()
+      }
+   })
+   
+   scheduleSubmit.onclick = function () {
+      if (!scheduleCheck) return
+      const Data = scheduleCollect()
+      if (!Data) return
+      // return console.log(Data)
+      SCHEDULE.save(Data)
+      window.location.reload()
    }
+   
+   
+   timeSubmit.onclick = async function () {
+      let param = true, length = false
+      document.querySelectorAll('.time-grup .time-code').forEach(code => {
+         length = true
+         code.borderColor = 'darkcyan'
+         if (code.value != '') return
+         param = false
+         code.borderColor = 'red'
+      })
+      if (!param || !length) return alert('Input schedule code')
+      if (!confirm('Update ?')) return
+      await SCHEDULE.saveTime(timeCollect())
+      window.location.reload()
+   }
+   
+   timeHead.onclick = function () {
+      let param = true
+      document.querySelectorAll('.time-grup .time-code').forEach(code => {
+         if (code.value != '') return
+         param = false
+         code.borderColor = 'red'
+      })
+      if (!param) return alert('Input schedule code')
+      const element = document.createElement('div')
+      element.classList.add('time-grup')
+      element.innerHTML = `
+         <input type="text" class='time-code bg-darkcyan' value=''>
+         <input type="text" class='time-value' value=''>
+         <select onchange='timeColor(this)'>
+            <option value='darkcyan'>Darkcyan</option>
+            <option value='red'>Red</option>
+            <option value='blue'>Blue</option>
+            <option value='green'>Green</option>
+            <option value='yellow'>Yellow</option>
+            <option value='black'>Black</option>
+            <option value='purple'>Purple</option>
+         </select>
+         <input type='checkbox' class='check-form' data-text='Use ?'>
+         <i class="fas fa-trash" onclick="timeRemove(this)"></i>
+      `
+      scheduleTime.appendChild(element)
+   }
+   
 })
 
-if (localStorage.getItem("schedule")) {
-   readSch()
-}
-
-function updateSchedule () {
+function scheduleCheck () {
    let param = true
-   document.querySelectorAll("#schedule-set select").forEach(sl => {
-      if (sl.value == '') return param = false
+   document.querySelectorAll('select.schedule-select').forEach(select => {
+      console.log(select.value)
+      if(select.value == '') param = false
    })
-   if (param == false) return confirm("Choose every option !")
-   if (!confirm("Update Schedule ?")) return
-   
-   let month   = new Date().getMonth()
-   let year    = new Date().getFullYear()
-   let setSch  = "schedule" + month + '' + year
-   
-   let SchArray = []
-   for (i = 1; i <= lastDatex; i++) {
-      const id    = "#sl" + i
-      const value  = document.querySelector(`${id}`).value
-      // console.log(value)
-      SchArray.push(value)
-   } 
-   localStorage.setItem(setSch, JSON.stringify(SchArray))
-   return window.location.reload()
+   if (!param) alert('Choose your schedule')
+   return param
+}
+function findSchedule (date = new Date()) {
+   const month = new Date(date).getMonth(),
+      year     = new Date(date).getFullYear(),
+      index    = SCHEDULE.allData.findIndex(({code}) => code.toUpperCase() == `${month}${year}`)
+      if (index >= 0) return true
+      return false
 }
 
-function readSch () {
+function scheduleCollect () {
+   if (!scheduleCheck()) return undefined
+   let Data       = [],
+      param       = true
+   const month    = new Date().getMonth(),
+      year        = new Date().getFullYear(),
+      lastDate    = new Date(year, month + 1, 0).getDate()
    
-   let month         = new Date().getMonth()
-   let year          = new Date().getFullYear()
-   let setSch        = "schedule" + month + '' + year
-   let ScheduleNow   = JSON.parse(localStorage.getItem(setSch))
-   
-   if (ScheduleNow.length < 1) return
-   
-   // console.log(ScheduleNow)
-   let HTML = 
-   ScheduleNow.forEach((data, i) => {
-      i++
-      let x = "#sl"+ i
-      const sl = document.querySelector(x)
-      sl.querySelectorAll("option").forEach(opt => {
-         opt.selected = false
-         if (opt.value == data) {
-            opt.selected = true
-            let color = opt.dataset.color
-            sl.parentElement.firstElementChild.style.background = color
-         }
+   for (let i = 1; i <= lastDate; i++){
+      const elem  = document.querySelector(`#sch${i}`)
+      if (!elem) param = false
+      else Data.push(elem.value)
+   }
+   if (param) return Data
+   return undefined
+}
+
+
+function timeCollect () {
+   let data = []
+   document.querySelectorAll('.time-grup').forEach(time => {
+      data.push({
+         code  : time.firstElementChild.value,
+         color : time.querySelector('select').value,
+         time  : time.querySelector('.time-value').value,
+         use   : time.querySelector('.check-form').checked
       })
    })
+   return data
+}
+async function timeRemove(elm) {
+   if (!confirm('This action will remove this schedule code in every month schedule list!')) return
+   const parent   = elm.parentElement
+   const code     = parent.dataset.code
+   await SCHEDULE.timeRemoves(code)
+   parent.remove()
 }
